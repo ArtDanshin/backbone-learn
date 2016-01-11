ba94dcb38979ae6963c728844afbcd6e881aeb04
SilverDragoon <silvir007@gmail.com>
2016-01-11 13:14:40 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Для запуска проекта необходим установленный Ruby и compass
+
+# Как установить
+
+1. git clone https://github.com/SilverDragoon/LoftSchool_Shop.git fileupload
+2. cd !$
+3. bower i
+4. npm i
+5. gulp
+
+
---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+var sdProject = (function () {
+
+	// Инициализация
+	var init = function () {
+		_setUpListners();
+	};
+
+	// Прослушивание событий
+	var _setUpListners = function () {
+		
+	};
+
+	return {
+		init: init
+	}
+})();
+
+sdProject.init();
